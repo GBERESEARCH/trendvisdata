623687d43b55b9cf574356b5ea825db3f091472b
GBERESEARCH <68221707+GBERESEARCH@users.noreply.github.com>
2026-03-20 16:52:56 +0000

    Update trend_params

--- trendvisdata/trend_params.py
+++ trendvisdata/trend_params.py
@@ -252,7 +252,7 @@ USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 '
-    'Safari/537.36'
+    'Safari/537.36',
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 "
     "Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 "
